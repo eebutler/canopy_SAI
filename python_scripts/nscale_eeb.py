928e8b7a2c2193cc3ffaa5ad33e1fb246adc85dc
eebutler <eebutler@outlook.com>
2020-09-14 10:03:52 -0500

    updated variable names

--- python_scripts/nscale_eeb.py
+++ python_scripts/nscale_eeb.py
@@ -46,10 +46,10 @@ Fsun4 = na_osun(lai,Kb(0,chi),0.3)/(fsun(lai,0.5,Kb(0,chi))*lai)
 plt.plot(lai,[1]*len(lai),'k')
 plt.plot(lai,Fsun1,'g--',label='zenith = 70$^{\circ}$, SAI = 0.5')
 plt.plot(lai,Fsun2,':',color='brown',label='zenith = 0$^{\circ}$  , SAI = 5.0')
-#plt.plot(lai,Fsun3,'--',color='brown',label='zenith = 70$^{\circ}$, SAI = 5.0')
-#plt.plot(lai,Fsun4,'g:',label='zenith = 0$^{\circ}$  , SAI = 0.5')
+plt.plot(lai,Fsun3,'--',color='brown',label='zenith = 70$^{\circ}$, SAI = 5.0')
+plt.plot(lai,Fsun4,'g:',label='zenith = 0$^{\circ}$  , SAI = 0.5')
 plt.xlabel('LAI')
-plt.ylabel('Sunlit $N_{a}$')
+plt.ylabel('Sunlit $NSCALE_{a}$')
 plt.legend()
 plt.savefig('na_old.pdf')
 plt.close('all')
@@ -67,7 +67,7 @@ Nscaler_osun = nscaler_osun/(Fsun*lai)
 Nscaler_osha = nscaler_osha/((1-Fsun)*lai)
 
 ## new Fsun
-Fsun_new = (1-np.exp(-kb*lai))*(1-np.exp(-kb*sai))/(kb*kb*(sai+lai))
+Fsun_new = (1-np.exp(-kb*lai))*(1-np.exp(-kb*sai))/(kb*kb*(sai*lai))
 
 Nscaler_sun = ( ((1-np.exp(-kb*sai))/kb) * (1-np.exp(-(kn+kb)*lai))/(kn+kb) )/(Fsun_new*lai*sai) 
 Nscaler_sha = ( sai*( 1-np.exp(-kn*lai) )/kn - \
@@ -97,7 +97,7 @@ plt.plot(lai,Nscaler_sun,'g',label='new sun')
 plt.plot(lai,Nscaler_sha,'b',label='new shade')
 plt.plot(lai,Nscaler_whole,'r:',label='whole canopy')
 plt.xlabel('LAI')
-plt.ylabel('N$_{a}$')
+plt.ylabel('NSCALE$_{a}$')
 plt.legend()
 plt.xlim([0,6])
 plt.ylim([0,1.5])
@@ -106,25 +106,25 @@ plt.close('all')
 
 
 #### a little bit more to play with not currently displayed in manuscript
-def fsun_new(lai,sai,kb):
-    fsu_new = (1-np.exp(-kb*lai))*(1-np.exp(-kb*sai))/(kb*kb*sai*lai)
-    return fsu_new
-
-def na_sun(lai,sai,kb,kn):
-    na = ( ((1-np.exp(-kb*sai))/kb) * (1-np.exp(-(kn+kb)*lai))/(kn+kb) )
-    return na
-
-chi = 0
-Fsun1 = na_osun(lai,Kb(70,chi),0.3)/(fsun(lai,0.5,Kb(70,chi))*lai)
-Fsun2 = na_osun(lai,Kb(0,chi),0.3)/(fsun(lai,5,Kb(0,chi))*lai)
-Fsun3 = na_sun(lai,0.5,Kb(70,chi),0.3)/(fsun_new(lai,0.5,Kb(70,chi))*lai*0.5)
-Fsun4 = na_sun(lai,5.0,Kb(0,chi),0.3)/(fsun_new(lai,5,Kb(0,chi))*lai*5)
-
-plt.plot(lai,Fsun1,'g--',label='Additive, zenith = 70$^{\circ}$, SAI = 0.5')
-plt.plot(lai,Fsun2,'g:',label='Additive, zenith = 0$^{\circ}$  , SAI = 5.0')
-plt.plot(lai,Fsun3,'b--',label='Multiplicative, zenith = 70$^{\circ}$, SAI = 0.5')
-plt.plot(lai,Fsun4,'b:',label='Multiplicative, zenith = 0$^{\circ}$  , SAI = 5.0')
-plt.xlabel('LAI')
-plt.ylabel('Sunlit N$_{a}$')
-plt.legend()
+#def fsun_new(lai,sai,kb):
+#    fsu_new = (1-np.exp(-kb*lai))*(1-np.exp(-kb*sai))/(kb*kb*sai*lai)
+#    return fsu_new
+#
+#def na_sun(lai,sai,kb,kn):
+#    na = ( ((1-np.exp(-kb*sai))/kb) * (1-np.exp(-(kn+kb)*lai))/(kn+kb) )
+#    return na
+#
+#chi = 0
+#Fsun1 = na_osun(lai,Kb(70,chi),0.3)/(fsun(lai,0.5,Kb(70,chi))*lai)
+#Fsun2 = na_osun(lai,Kb(0,chi),0.3)/(fsun(lai,5,Kb(0,chi))*lai)
+#Fsun3 = na_sun(lai,0.5,Kb(70,chi),0.3)/(fsun_new(lai,0.5,Kb(70,chi))*lai*0.5)
+#Fsun4 = na_sun(lai,5.0,Kb(0,chi),0.3)/(fsun_new(lai,5,Kb(0,chi))*lai*5)
+#
+#plt.plot(lai,Fsun1,'g--',label='Additive, zenith = 70$^{\circ}$, SAI = 0.5')
+#plt.plot(lai,Fsun2,'g:',label='Additive, zenith = 0$^{\circ}$  , SAI = 5.0')
+#plt.plot(lai,Fsun3,'b--',label='Multiplicative, zenith = 70$^{\circ}$, SAI = 0.5')
+#plt.plot(lai,Fsun4,'b:',label='Multiplicative, zenith = 0$^{\circ}$  , SAI = 5.0')
+#plt.xlabel('LAI')
+#plt.ylabel('Sunlit NSCALE$_{a}$')
+#plt.legend()
 #plt.show()
